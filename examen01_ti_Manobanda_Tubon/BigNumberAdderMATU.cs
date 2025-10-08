3949ec640caacbff8348507c25db5e2e2ee2796e
anamanobanda1-d <anamanobanda20191714@gmail.com>
2025-10-08 11:38:12 -0500

    Creacion de la sintaxis en StringNumberMATU

--- examen01_ti_Manobanda_Tubon/BigNumberAdderMATU.cs
+++ examen01_ti_Manobanda_Tubon/BigNumberAdderMATU.cs
@@ -1,63 +1,45 @@
-﻿using System.Text;
-
-namespace examen01_ti_Manobanda_Tubon;
+﻿
+namespace examen01_ti_manobanda_tubon;
 
 public class BigNumberAdderMATU : IAdderMATU
 {
     public string AddMATU(string numberA_MATU, string numberB_MATU)
     {
-        if (numberA_MATU == null || numberB_MATU == null)
-            throw new ArgumentNullException("Los números no pueden ser nulos.");
-
-        if (!IsDigitsMATU(numberA_MATU) || !IsDigitsMATU(numberB_MATU))
-            throw new ArgumentException("Los valores deben contener solo dígitos (0-9).");
-
         var a_MATU = ReverseMATU(numberA_MATU);
         var b_MATU = ReverseMATU(numberB_MATU);
 
-        var maxLen_MATU = Math.Max(a_MATU.Length, b_MATU.Length);
-        var carry_MATU = 0;
-        var sb_MATU = new StringBuilder();
+        int maxLen_MATU = Math.Max(a_MATU.Length, b_MATU.Length);
+        int carry_MATU = 0;
+        StringBuilder sb_MATU = new StringBuilder();
 
         for (int i = 0; i < maxLen_MATU; i++)
         {
             int da_MATU = (i < a_MATU.Length) ? (a_MATU[i] - '0') : 0;
             int db_MATU = (i < b_MATU.Length) ? (b_MATU[i] - '0') : 0;
 
-            int s_MATU = da_MATU + db_MATU + carry_MATU;
-            carry_MATU = s_MATU / 10;
-            int digit_MATU = s_MATU % 10;
+            int sum_MATU = da_MATU + db_MATU + carry_MATU;
+            carry_MATU = sum_MATU / 10;
+            int digit_MATU = sum_MATU % 10;
             sb_MATU.Append((char)('0' + digit_MATU));
         }
 
         if (carry_MATU > 0) sb_MATU.Append((char)('0' + carry_MATU));
 
-        var res_MATU = ReverseMATU(sb_MATU.ToString());
-        res_MATU = TrimLeadingZerosMATU(res_MATU);
-        return res_MATU;
+        return TrimLeadingZerosMATU(ReverseMATU(sb_MATU.ToString()));
     }
 
-    private static bool IsDigitsMATU(string s_MATU)
+    private string ReverseMATU(string s_MATU)
     {
-        if (string.IsNullOrEmpty(s_MATU)) return false;
-        foreach (var c in s_MATU)
-        {
-            if (c < '0' || c > '9') return false;
-        }
-        return true;
+        char[] arr = s_MATU.ToCharArray();
+        Array.Reverse(arr);
+        return new string(arr);
     }
 
-    private static string ReverseMATU(string s_MATU)
-    {
-        var arr_MATU = s_MATU.ToCharArray();
-        Array.Reverse(arr_MATU);
-        return new string(arr_MATU);
-    }
-
-    private static string TrimLeadingZerosMATU(string s_MATU)
+    private string TrimLeadingZerosMATU(string s_MATU)
     {
         int i = 0;
         while (i < s_MATU.Length - 1 && s_MATU[i] == '0') i++;
         return s_MATU.Substring(i);
     }
 }
+
--- examen01_ti_Manobanda_Tubon/StringNumberMATU.cs
+++ examen01_ti_Manobanda_Tubon/StringNumberMATU.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace examen01_ti_Manobanda_Tubon;
 
-namespace examen01_ti_Manobanda_Tubon
+public class StringNumberMATU
 {
-    internal class StringNumberMATU
+    public string OriginalMATU { get; }
+    public string NormalizedMATU { get; }
+
+    public StringNumberMATU(string input_MATU)
     {
+        if (string.IsNullOrWhiteSpace(input_MATU))
+            throw new ArgumentException("El valor no puede estar vacío.");
+
+        string s_MATU = input_MATU.Trim();
+
+        if (s_MATU.StartsWith("+")) s_MATU = s_MATU.Substring(1);
+        if (s_MATU.StartsWith("-"))
+            throw new ArgumentException("Solo se aceptan enteros positivos.");
+
+        foreach (char c in s_MATU)
+        {
+            if (c < '0' || c > '9') throw new ArgumentException("El valor debe contener solo dígitos.");
+        }
+
+        int idx_MATU = 0;
+        while (idx_MATU < s_MATU.Length - 1 && s_MATU[idx_MATU] == '0') idx_MATU++;
+        NormalizedMATU = s_MATU.Substring(idx_MATU);
+        OriginalMATU = input_MATU;
     }
 }
