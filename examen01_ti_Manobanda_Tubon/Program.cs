57a705776faa5ee0bb8a285460058d928a15ad0e
anamanobanda1-d <anamanobanda20191714@gmail.com>
2025-10-08 10:33:52 -0500

    Agregar archivos de proyecto.

---
+++ examen01_ti_Manobanda_Tubon/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace examen01_ti_Manobanda_Tubon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
